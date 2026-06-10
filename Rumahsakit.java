125a9fa7026b29eb5c9911b9c8f4edffc1ad8682
breachcipher <lynxsaiko7@gmail.com>
2026-06-10 22:26:09 +0700

    Update Rumahsakit.java

--- Rumahsakit.java
+++ Rumahsakit.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package rumahsakit;
 
-/**
- *
- * @author Advan
- */
-public class Rumahsakit {
+import tampilan.form_login;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Rumahsakit {
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Langsung buka form login
+        new form_login().setVisible(true);
     }
-    
 }
